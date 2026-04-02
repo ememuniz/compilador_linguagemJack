8fd32eef8941ba63abe9d4100e416bbdcdf7057f
Emerson Paulo Pinheiro Muniz <emerson.muniz@discente.ufma.br>
2026-04-02 19:40:28 -0300

    Testes para reconhecimento de números (falhando de propósito - a implementação das funções será feita posteriormente

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@
 
 std::string TESTFILE = "test_codigo.jack";
 std::string TESTSYMBOL = "test_symbol.jack";
+std::string TESTNUMBER = "test_number.jack";
 
 //Teste para verificar se o tokenizer leu o arquivo certo
 TEST_CASE("Testando a leitura de arquivos"){
@@ -18,9 +19,26 @@ TEST_CASE("Testando o reconhecimento de Simbolos"){
 
   tokenizer.advance();                          //Avança para o primeiro token e armazena ele e seu tipo em suas respectivas variáveis
   CHECK(tokenizer.getToken() == "{");           //Verifica se o primeiro token foi lido
-  CHECK(tokenizer.tokenType() == SYMBOL);       //Verifica se o token lido foi um simbolo
+  CHECK(tokenizer.tokenType() == SYMBOL); 
+  
+  tokenizer.advance();
+  CHECK(tokenizer.getToken() == ">");
+  CHECK(tokenizer.tokenType() == SYMBOL);
 
   tokenizer.advance();                          //Avança para o segundo token e armazena ele e seu tipo em suas respectivas variáveis
   CHECK(tokenizer.getToken() == "}");           //Verifica se o segundo token foi lido
   CHECK(tokenizer.tokenType() == SYMBOL);       //Verifica se o token lido foi um simbolo
+}
+
+//Teste para verificar se os números estão sendo lidos corretamente
+TEST_CASE("Testando o reconhecimento de Números"){
+  JackTokenizer tokenizer(TESTNUMBER);         //Cria um tokenizer com o arquivo de teste
+
+  tokenizer.advance();                         //Avança para o primeiro token e armazena ele e seu tipo em suas respectivas variáveis
+  CHECK(tokenizer.getToken() == "123");        //Verifica se o primeiro token foi lido
+  CHECK(tokenizer.tokenType() == INT_CONST);   //Verifica se o token lido foi um inteiro
+
+  tokenizer.advance();                         //Avança para o segundo token e armazena ele e seu tipo em suas respectivas variáveis
+  CHECK(tokenizer.getToken() == "45");        //Verifica se o segundo token foi lido
+  CHECK(tokenizer.tokenType() == INT_CONST);   //Verifica se o token lido foi um inteiro
 }
