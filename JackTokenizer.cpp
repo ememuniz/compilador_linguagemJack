b5efaf474ce8aa9de05f7cc89985e7172fcec4c9
Emerson Paulo Pinheiro Muniz <emerson.muniz@discente.ufma.br>
2026-04-02 17:16:37 -0300

    Implementa advance() básico para ler simbolos e pular espaços em branco.

--- JackTokenizer.cpp
+++ JackTokenizer.cpp
@@ -14,10 +14,43 @@ JackTokenizer::JackTokenizer(std::string filename){
   } else {
     input_code = ""; // Se der erro, fica vazio
   }
+
+  //variaveis
+  position = 0;               //Armazena a posicao atual do arquivo, caractere a caractere
+  currentToken = "";          //Armazena o token atual
+  currentTokenType = NONE;    //Armazena o tipo de token atual
 }
 
+//Função para verificar se leu o arquivo todo
+bool JackTokenizer::hasMoreTokens(){return position < input_code.length();} //Retorna True se a posição atual for menor que o tamanho total do arquivo. Se nao, retorna False
 
+
+void JackTokenizer::advance(){
+  currentToken = "";          //Limpa o token atual
+
+  while(position < input_code.length() && input_code[position] == ' '){
+    position++;
+  }                           //Enquanto ainda tiver caracteres a serem lidos e for um espaço, avance.
+
+  if (!hasMoreTokens()) return; //Se ja estiver terminado o texto do arquivo, finalize.
+
+  //Variáveis de armazenamento
+  char c = input_code[position];                  //Armazena o caractere na posicao atual
+  std::string symbols = "{}()[].,;+-*/&|<>=~";    //Armazena os simbolos
+
+  if (symbols.find(c) != std::string::npos){     //Se o caractere atual for um simbolo
+    currentToken = c;                            //Adiciona o caractere ao token atual
+    currentTokenType = SYMBOL;                   //Define o tipo de token atual como simbolo
+    position++;                                  //Avance para o proximo caractere
+    return;                                      //Finaliza a funcao
+  }
+
+  //Temporario para não ficar em looping
+  position++;
+}
 
 //Função de teste
-std::string JackTokenizer::getCode(){return input_code;} //Retorna o conteudo do arquivo
+std::string JackTokenizer::getCode(){return input_code;}           //Retorna o conteudo do arquivo
+TokenTypeList JackTokenizer::tokenType(){return currentTokenType;} //Retorna o tipo de token atual
+std::string JackTokenizer::getToken(){return currentToken;}        //Retorna o token atual
 
--- test.cpp
+++ test.cpp
@@ -2,9 +2,25 @@
 #include "doctest.h"
 #include "JackTokenizer.h"
 
-std::string TEST = "test_codigo.jack";
+std::string TESTFILE = "test_codigo.jack";
+std::string TESTSYMBOL = "test_symbol.jack";
 
+//Teste para verificar se o tokenizer leu o arquivo certo
 TEST_CASE("Testando a leitura de arquivos"){
-  JackTokenizer tokenizer(TEST); //Cria um tokenizer com o arquivo de teste
+  JackTokenizer tokenizer(TESTFILE); //Cria um tokenizer com o arquivo de teste
   CHECK(tokenizer.getCode() == "class Main{ }"); //Verifica se o tokenizer leu o arquivo certo
+}
+
+//Teste para verificar se os simbolos estão sendo lidos corretamente
+TEST_CASE("Testando o reconhecimento de Simbolos"){
+  JackTokenizer tokenizer("test_symbol.jack");  //Cria um tokenizer com o arquivo de teste
+  CHECK(tokenizer.hasMoreTokens() == true);     //Verifica se o texto foi lido todo.  
+
+  tokenizer.advance();                          //Avança para o primeiro token e armazena ele e seu tipo em suas respectivas variáveis
+  CHECK(tokenizer.getToken() == "{");           //Verifica se o primeiro token foi lido
+  CHECK(tokenizer.tokenType() == SYMBOL);       //Verifica se o token lido foi um simbolo
+
+  tokenizer.advance();                          //Avança para o segundo token e armazena ele e seu tipo em suas respectivas variáveis
+  CHECK(tokenizer.getToken() == "}");           //Verifica se o segundo token foi lido
+  CHECK(tokenizer.tokenType() == SYMBOL);       //Verifica se o token lido foi um simbolo
 }
