7514cb41af3e54e1caea8049c74e1d53c063aa67
Emerson Paulo Pinheiro Muniz <emerson.muniz@discente.ufma.br>
2026-04-20 03:14:22 -0300

    adicionado o teste de parser - o teste ainda não está passando

---
+++ include/CompilationEngine.h
@@ -0,0 +1,16 @@
+#ifndef COMPILATION_ENGINE_H
+#define COMPILATION_ENGINE_H
+
+#include "JackTokenizer.h"
+#include <fstream>
+#include <string>
+#include <stdexcept>
+
+
+
+
+class CompilationEngine {
+
+};
+
+#endif
---
+++ tests/test_parser.cpp
@@ -0,0 +1,36 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "JackTokenizer.h"
+#include "CompilationEngine.h"
+#include <fstream>
+
+TEST_CASE("Testando ferramentas auxiliares do Compilation Engine") {
+
+  //#region MARK: PREPARAÇÃO DO ARQUIVO DE TESTE
+  std::ofstream out("temp_test.jack");                      //Cria o arquivo de teste
+  out << "class Main{ }";                                   //Escreve no arquivo
+  out.close();                                              //Fecha o arquivo
+  //#endregion
+
+  //region MARK: INICIA O SCANNER E O PARSER
+  JackTokenizer tokenizer("temp_test.jack");                //Cria um tokenizer com o arquivo de teste
+  CompilationEngine engine(tokenizer, "temp_test.xml");     //Cria um engine com o tokenizer e o arquivo de saída, uma engine é um parser
+  //#endregion
+
+  //region MARK: TESTE DO MATCH()
+  SUBCASE("Verifica se match() funciona corretamente"){
+    CHECK(engine.match(KEYWORD) == true);                   //Verifica se a keyword foi lida
+    CHECK(engine.match(KEYWORD, "class") == true);          //Verifica se a keyword "class" foi lida
+    CHECK(engine.match(SYMBOL) == false);                   //Verifica se o simbolo foi lido
+  }
+  //endregion
+
+  //region MARK: TESTE DO CONSUME()
+  SUBCASE("Verifica se consume() consome o token correto e lança erro no incorreto"){
+    CHECK_NOTHROW(engine.consume(KEYWORD, "class"));        //Verifica se "class" é consumida
+    CHECK(engine.match(IDENTIFIER, "Main") == true);        //O token atual deve ser 'Main
+    CHECK_THROWS_AS(engine.consume(SYMBOL, "{"), std::runtime_error); //Se um símbolo for consumido agora, é lançado um erro
+  }
+  //endregion
+  
+}
